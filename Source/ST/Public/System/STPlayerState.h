9396be475e7f4f72215df74baad22f474cbcc776
JangJuMan <wnaks9901@gmail.com>
2025-08-04 19:39:51 +0900

    [Add] STPlayerState
    플레이어 상태를 저장하는 곳
    
    float Score
    int32 KillCount
    SelectedCharacter
    
    STPlayerState.h
    STPlayerState.cpp
    BP_STPlayerState.uasset

---
+++ Source/ST/Public/System/STPlayerState.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "STGameTypes.h"
+#include "GameFramework/PlayerState.h"
+#include "STPlayerState.generated.h"
+
+/**
+ *  플레이어의 개별 정보 저장
+ */
+UCLASS()
+class ST_API ASTPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	ASTPlayerState();
+
+	/*중복선언 에러*/
+	/*UPROPERTY( BlueprintReadWrite, Category="PlayerState" ) 
+	int32 Score;*/
+
+	UPROPERTY( BlueprintReadWrite, Category="PlayerState" )
+	int32 KillCount;
+
+	UPROPERTY( BlueprintReadWrite, Category="PlayerState" )
+	ECharacterType SelectedCharacter;
+
+	UFUNCTION( BlueprintCallable )
+	void ResetPlayerState();
+};
