12c81a0871423e9f7509e9727352ea0b7c291160
JangJuMan <wnaks9901@gmail.com>
2025-08-04 19:37:09 +0900

    [Add] STGameState
    게임 진행 상태를 저장
    GameMode에서만 값을 수정하기. 다른곳에선 읽기만 하기
    (그러나 수정에 대한 코드적 제약 아직 없음)
    
    -StagePhase, RemainingEnemies, RemainingTime, BossPhase, StateResult
    
    STGameState.h
    STGameState.cpp
    BP_STGameState.uasset

---
+++ Source/ST/Private/System/STGameState.cpp
@@ -0,0 +1,35 @@
+﻿#include "System/STGameState.h"
+
+ASTGameState::ASTGameState()
+{
+	StagePhase = EStagePhase::Start;
+	RemainingEnemies = 0;
+	RemainingTime = 0.0f;
+	BossPhase = 1;
+	StageResult = EStageResult::None;
+}
+
+void ASTGameState::SetStagePhase(EStagePhase NewPhase)
+{
+	StagePhase = NewPhase;
+}
+
+void ASTGameState::SetRemainingEnemies(int32 NewCount)
+{
+	RemainingEnemies = NewCount;
+}
+
+void ASTGameState::SetRemainingTime(float NewTime)
+{
+	RemainingTime = NewTime;
+}
+
+void ASTGameState::SetBossPhase(int32 NewPhase)
+{
+	BossPhase = NewPhase;
+}
+
+void ASTGameState::SetStageResult(EStageResult NewResult)
+{
+	StageResult = NewResult;
+}
