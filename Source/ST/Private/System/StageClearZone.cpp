b677323802fcce6401d6d9cdb2083bf2513c57b3
JangJuMan <wnaks9901@gmail.com>
2025-08-05 15:51:38 +0900

    [Add] StageClearZone 생성
    모든 적 섬멸 후, 해당 존에 들어가면 다음 스테이지로 넘어갈 수 있음.
     -> 추후 Particle 등 시각적 효과 추가 가능
    
    StageClearZone.h
    StageClearZone.cpp
    BP_StageClearZone.uasset

---
+++ Source/ST/Private/System/StageClearZone.cpp
@@ -0,0 +1,39 @@
+﻿#include "System/StageClearZone.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "System/STGameMode.h"
+
+
+AStageClearZone::AStageClearZone()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Scene = CreateDefaultSubobject<USceneComponent>("Scene");
+	RootComponent = Scene;		// 생성자에서 루트 지정시
+	// SetRootComponent(Scene); // 런타임에 루트 변경시
+	
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	TriggerBox->SetCollisionProfileName("Trigger");
+	TriggerBox->SetupAttachment(Scene);
+}
+
+void AStageClearZone::BeginPlay()
+{
+	Super::BeginPlay();
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AStageClearZone::OnOverlapBegin);	
+}
+
+void AStageClearZone::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && OtherActor->IsA(ACharacter::StaticClass()))	// TODO: STPlayerCharacter 클래스로 변경?
+	{
+		if (ASTGameMode* STGameMode = Cast<ASTGameMode>(GetWorld()->GetAuthGameMode()))
+		{
+			STGameMode->CheckStageClear();
+		}
+	}
+}
+
+
---
+++ Source/ST/Public/System/StageClearZone.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "StageClearZone.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class ST_API AStageClearZone : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AStageClearZone();
+
+protected:
+	UPROPERTY( VisibleAnywhere )
+	USceneComponent* Scene;
+	
+	UPROPERTY( VisibleAnywhere )
+	UBoxComponent* TriggerBox;
+	
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+};
