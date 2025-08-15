08bf7b3e7bc5d7152e7e19e31678e5476acd52be
Parkjunbeom99 <pjb990@gmail.com>
2025-08-15 18:11:00 +0900

    [Update ]1인칭 캐릭터 equip 애니메이션 재생

--- Source/ST/Private/Player/STWeaponManagerComponent.cpp
+++ Source/ST/Private/Player/STWeaponManagerComponent.cpp
@@ -43,9 +43,10 @@ void USTWeaponManagerComponent::RequestEquipWeapon(TSoftClassPtr<ASTWeaponBase> 
 	
 	bIsWeaponChanged = true;
 	UAnimInstance* AnimInstance = OwnerChar->GetMesh()->GetAnimInstance();
+	UAnimInstance* FPSAnimInstance = OwnerChar->GetFPSSkeletalMesh()->GetAnimInstance();
 	UAnimMontage* EquipMontage = OwnerChar->GetMontageConfig()->EquipMontage;
 
-	if (!AnimInstance)
+	if (!AnimInstance&&!FPSAnimInstance)
 	{
 		return;
 	}
@@ -54,8 +55,16 @@ void USTWeaponManagerComponent::RequestEquipWeapon(TSoftClassPtr<ASTWeaponBase> 
 		return;
 	}
 	AnimInstance->Montage_Play(EquipMontage);
+	FPSAnimInstance->Montage_Play(EquipMontage);
 	MontageEndedDelegate.BindUObject(this, &USTWeaponManagerComponent::OnEquipMontageEnded);
-	AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, EquipMontage);
+	if (OwnerChar->GetCurrentViewMode() == EViewMode::TPS)
+	{
+		AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, EquipMontage);
+	}
+	else
+	{
+		FPSAnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, EquipMontage);
+	}
 	
 	
 }
