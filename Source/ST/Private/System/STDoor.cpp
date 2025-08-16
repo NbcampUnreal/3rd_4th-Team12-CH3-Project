3c7903d99bcd5a5b81e2d800f8f89c474c41bf53
JangJuMan <wnaks9901@gmail.com>
2025-08-16 14:09:32 +0900

    [Update] Stage3 보스방 막는 문 배치
    보스방 진입시 문 닫는 기능
     - Trigger에 닿으면 문 닫힘 (트리거BP는 1번 작동 후, Destroy)
    
    보스 처치시 문 열림
     - 보스 Enemy->OnDied 이벤트에 바인딩
    
    문 열리는건 BP_Door의 이벤트 그래프에서, Timeline으로 애니메이션 구현

---
+++ Source/ST/Private/System/STDoor.cpp
@@ -0,0 +1,55 @@
+﻿#include "System/STDoor.h"
+
+#include "Enemy/STEnemyBase.h"
+#include "Enemy/STEnemyBoss.h"
+#include "Kismet/GameplayStatics.h"
+#include "System/STLog.h"
+
+
+ASTDoor::ASTDoor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>("DoorMesh");
+	RootComponent = DoorMesh;
+
+	bIsClosed = false;
+}
+
+void ASTDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> Enemies;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASTEnemyBase::StaticClass(), Enemies);
+	for (AActor* E : Enemies)
+	{
+		if (ASTEnemyBoss* Boss = Cast<ASTEnemyBoss>(E))
+		{
+			Boss->OnDied.AddDynamic(this, &ASTDoor::HandleBossDefeated);
+			UE_LOG(LogSystem, Warning, TEXT("ASTDoor::BeginPlay() Binding Success : HandleBossDefeated"));
+		}
+		else
+		{
+			UE_LOG(LogSystem, Warning, TEXT("ASTDoor::BeginPlay() Binding Failed : HandleBossDefeated"));
+		}
+	}
+}
+
+void ASTDoor::HandleBossDefeated(AActor* DeadEnemy)
+{
+	
+	if (Cast<ASTEnemyBoss>(DeadEnemy))
+	{
+		if (bIsClosed)
+		{
+			OpenDoor();
+			bIsClosed = false;
+			UE_LOG(LogSystem, Warning, TEXT("ASTDoor::HandleBossDefeated() Door Opened"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogSystem, Warning, TEXT("ASTDoor::HandleBossDefeated() Cast Failed"));
+		
+	}
+}
